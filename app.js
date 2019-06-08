00c8b27912f4b4a16dcc26794f14efd24ccf2662
Arabind <arabind_97@hotmail.com>
2019-06-08 21:54:50 +0100

    Timer check

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ let today = new Date()
 let formatDate = today.toDateString()
 let selectElement = document.getElementById('date')
 selectElement.innerHTML = formatDate
+
+var timer = 10;
+
+var interval = setInterval(function() {
+    timer--;
+    $('.timer').text(timer);
+    if (timer === 0) clearInterval(interval);
+}, 1000);
 console.log('Here\' s a hidden message');
